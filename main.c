f4f9ed42c23916d8d29ff6c885935624f884de51
Felipe Sant'Ana <felipelimastn@gmail.com>
2025-10-04 16:03:46 -0300

    time counter of fibonnacti functions

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 #include "fibonacci_recursive.c"
 #include "fibonacci.c"
 
@@ -23,4 +24,31 @@ int main(int argc, char const *argv[])
 
     printf("fibonnaci(12) = %d\n", fibonacci(12));
     printf("fibonnaci_recursive(12) = %d\n", fibonacci_recursive(12));
+
+    int n2 = 20;
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    
+    printf("fibonnaci(%d) = %d\n", n2, fibonacci(n2));
+    
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    
+    uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+    printf("Time: %lu\n", delta_us);
+
+    // for(int i = 0; i < 10; i++) {
+    //     printf("i: %d\n", i);
+    // }
+
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    
+    printf("fibonnaci_recursive(%d) = %d\n", n2, fibonacci_recursive(n2));
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    
+    delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+    printf("Time: %lu\n", delta_us);
+
 }
