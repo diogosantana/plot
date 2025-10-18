50de2ffef1c9d6949793bc1e7a9c9278d3bcd0cf
Rafael Sant'Ana <rafaellimastn@gmail.com>
2025-10-18 12:13:34 -0300

    time counter of fibonacci functions with QueryPerfomanceCounter

--- main.c
+++ main.c
@@ -3,10 +3,17 @@
 #include <time.h>
 #include "fibonacci_recursive.c"
 #include "fibonacci.c"
+#include <Windows.h>
 
-int main(int argc, char const *argv[])   
+int main(int argc, char const *argv[])
 {
-    FILE* file = fopen("dist/test.txt", "w");
+    LARGE_INTEGER StartingTime, EndingTime, ElapsedMicroseconds;
+    LARGE_INTEGER Frequency;
+
+    QueryPerformanceFrequency(&Frequency);
+    QueryPerformanceCounter(&StartingTime);
+
+    FILE *file = fopen("dist/test.txt", "w");
 
     int n = 10;
 
@@ -14,30 +21,47 @@ int main(int argc, char const *argv[])
     {
         float y = 1 / (float)x;
         fprintf(file, "%d %f\n", x, y);
-        
+
         // int y = pow(2, x);
         // fprintf(file, "%d %d\n", x, y);
     }
     fclose(file);
 
-    printf("fibonnaci(12) = %d\n", fibonacci(12));
-    printf("fibonnaci_recursive(12) = %d\n", fibonacci_recursive(12));
+    QueryPerformanceCounter(&EndingTime);
+    ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
 
-    int n2 = 40;
+    ElapsedMicroseconds.QuadPart *= 1000000;
+    ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
+
+    printf("diff Arquivo: %lld us\n", ElapsedMicroseconds.QuadPart);
+
+    int n2 = 44;
 
     struct timespec start, end;
-    clock_t clock_start = clock();  
+    clock_t clock_start = clock();
 
     clock_gettime(CLOCK_MONOTONIC, &start);
     
+    QueryPerformanceCounter(&StartingTime);
+
     printf("fibonnaci(%d) = %d\n", n2, fibonacci(n2));
-    
+
+    QueryPerformanceCounter(&EndingTime);
+
     clock_gettime(CLOCK_MONOTONIC, &end);
-    clock_t clock_end = clock();  
+    clock_t clock_end = clock();
+
+    ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
+
+    ElapsedMicroseconds.QuadPart *= 1000000;
+    ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
+
+    printf("diff QPC: %lld us\n", ElapsedMicroseconds.QuadPart);
+
     clock_t diff = clock_end - clock_start;
 
     printf("diff: %lu ms\n", diff);
-    
+
     uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
     printf("Time: %lu us\n", delta_us);
 
@@ -45,13 +69,21 @@ int main(int argc, char const *argv[])
     //     printf("i: %d\n", i);
     // }
 
-    clock_start = clock();  
+    clock_start = clock();
     clock_gettime(CLOCK_MONOTONIC, &start);
-    
+    QueryPerformanceCounter(&StartingTime);
+
     printf("fibonnaci_recursive(%d) = %d\n", n2, fibonacci_recursive(n2));
 
+    QueryPerformanceCounter(&EndingTime);
+    ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
+
+    ElapsedMicroseconds.QuadPart *= 1000000;
+    ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
+    printf("diff QPC: %lld us\n", ElapsedMicroseconds.QuadPart);
+
     clock_gettime(CLOCK_MONOTONIC, &end);
-    
+
     clock_end = clock();
     diff = clock_end - clock_start;
     printf("diff: %lu ms\n", diff);
